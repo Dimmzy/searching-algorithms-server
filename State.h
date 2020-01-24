74605b5547a13c52e458c3af09ee09bfc90d2209
Dima <leetsushi@gmail.com>
2020-01-24 19:02:00 +0200

    matrix and stuff

---
+++ State.h
@@ -0,0 +1,60 @@
+#ifndef STATE_H_
+#define STATE_H_
+#include "string"
+template<typename T>
+class State {
+ private:
+  T state;
+  std::string state_name; // the state_name represented by a string
+  double cost; // the cost to cross this state_name
+  State<T> *cameFrom; // the state_name we came from to this state_name (setter)
+  double costFromInitial; //cost to reach this state_name (set by a setter)
+
+ public:
+  State<T>(std::string &state, double cost) {
+    this->state_name = state;
+    this->cost = cost;
+    this->cameFrom = nullptr;
+    this->costFromInitial = 0;
+  }
+
+  State<T>(T& state, double cost) {
+    this->state = state;
+    this->cost = cost;
+    this->cameFrom = nullptr;
+    this->costFromInitial = 0;
+  }
+
+  bool equals(State<T> &otherState) {
+    return this->state_name.compare(otherState.getState()) == 0;
+  }
+
+  void setCostFromInitial(const double cost) {
+    this->costFromInitial = cost;
+  }
+  double getCostFromInitial() {
+    return this->costFromInitial;
+  }
+
+  T getState() {
+    return this->state;
+  }
+
+  std::string getState_Name() {
+    return this->state_name;
+  }
+  double getCost() {
+    return this->cost;
+  }
+  void setCost(double cost) {
+    this->cost = cost;
+  }
+  State<T> *getPreviousNode() {
+    return this->cameFrom;
+  }
+  void setPreviousNode(State<T> *previousNode) {
+    this->cameFrom - previousNode;
+  }
+};
+
+#endif //STATE_H_
